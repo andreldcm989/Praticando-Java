a7f7ef48098444ead029f5bcbbb02eb8fd1d221f
Andre Cruz <andreldcruz@outlook.com>
2022-11-03 19:40:56 -0300

    Resolução exercicio 1

---
+++ 03112022/src/Exercicio1.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+/**
+ * @apiNote Implementar um sistema que lê um nome e apresente o nome em
+ *          maiúsculo, minúsculo, seu tamanho sem os espaços em branco e o
+ *          tamanho do primeiro nome.
+ * 
+ */
+public class Exercicio1 {
+    public static void main(String[] args) {
+        String nome1 = JOptionPane.showInputDialog(null, "Digite o primeiro nome");
+
+        JOptionPane.showMessageDialog(null, "Maiusculo: " + nome1.toUpperCase());
+        JOptionPane.showMessageDialog(null, "Minusculo: " + nome1.toLowerCase());
+        JOptionPane.showMessageDialog(null, "Total de letras: " + nome1.replace(" ", "").length());
+        JOptionPane.showMessageDialog(null,
+                "Letras no primeiro nome: " + nome1.substring(0, nome1.indexOf(" ")).length());
+    }
+}
