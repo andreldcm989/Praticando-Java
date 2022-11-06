30020a5f954200ab1e66397b3ebb0d9a4630c4de
Andre Cruz <andreldcruz@outlook.com>
2022-11-05 22:04:01 -0300

    Resolução exercicio 4

---
+++ 04112022/src/Exercicio4.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Recebo uma string com letras repetidas e
+ * imprimir a letra e sua quantidade;
+ * se tiver mais de 9 repetições, reinicia a contagem em um novo elemento
+ */
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+
+        String texto = JOptionPane.showInputDialog(null, "Entre com o texto:");
+        List<String> resultado = new ArrayList<>();
+        String letra = "";
+
+        for (int i = 0; i < texto.length(); i++) {
+            if (letra.contains(String.valueOf(texto.charAt(i))) || i == 0) {
+                letra += String.valueOf(texto.charAt(i));
+            } else {
+                resultado.add(letra);
+                letra = String.valueOf(texto.charAt(i));
+            }
+            if (i == texto.length() - 1) {
+                resultado.add(letra);
+            }
+
+        }
+        for (String s : resultado) {
+            if (s.length() > 9) {
+                System.out.println(s.substring(0, 1) + "9");
+                System.out.println(s.substring(0, 1) + (s.length() - 9));
+            } else {
+                System.out.println(s.substring(0, 1) + s.length());
+            }
+        }
+
+    }
+}
