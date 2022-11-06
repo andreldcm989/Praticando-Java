07b175859698256df8f906c2aa337979f75b90b5
Andre Cruz <andreldcruz@outlook.com>
2022-11-05 23:16:07 -0300

    resolução exercicio 4

---
+++ 05112022/src/Exercicio4.java
@@ -0,0 +1,13 @@
+
+public class Exercicio4 {
+    public static void main(String[] args) {
+        int soma = 0;
+
+        for (int i = 0; i <= 500; i += 3) {
+            if (i % 2 == 1)
+                soma += i;
+        }
+
+        System.out.println(soma);
+    }
+}
