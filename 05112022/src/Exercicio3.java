2802483d74983c58b90858085478032da877db5d
Andre Cruz <andreldcruz@outlook.com>
2022-11-05 23:09:07 -0300

    resolução exercicio 3

---
+++ 05112022/src/Exercicio3.java
@@ -0,0 +1,11 @@
+/**
+ * Imprimir os numeros pares entre 1 e 50
+ */
+public class Exercicio3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+}
