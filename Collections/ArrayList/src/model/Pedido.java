c6dc8dd3409079ead7c73462a23f1f4a2782d63e
Andre Cruz <andreldcruz@outlook.com>
2022-11-30 14:10:14 -0300

    exercicio com conjuntos Set usando hashset

--- Collections/ArrayList/src/model/Pedido.java
+++ Collections/ArrayList/src/model/Pedido.java
@@ -49,9 +49,40 @@ public class Pedido implements Comparable<Pedido> {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((destino == null) ? 0 : destino.hashCode());
+        result = prime * result + ((dataEntrega == null) ? 0 : dataEntrega.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pedido other = (Pedido) obj;
+        if (destino == null) {
+            if (other.destino != null)
+                return false;
+        } else if (!destino.equals(other.destino))
+            return false;
+        if (dataEntrega == null) {
+            if (other.dataEntrega != null)
+                return false;
+        } else if (!dataEntrega.equals(other.dataEntrega))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format("Destino: %s, Data entrega: %tF. Produtos: %n", destino, dataEntrega));
+        sb.append(String.format("Destino: %s, Data entrega: %2$td/%2$tm/%2$tY. Produtos: %n", destino, dataEntrega));
         produtos.forEach(p -> sb.append(p.toString()));
         sb.append(String.format("Total da nota: R$ %.2f %n", calculaNota()));
         sb.append("---------------------------------------------------------");
