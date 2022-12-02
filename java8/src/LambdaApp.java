bf449aed1d199cb5e5373971e8963f5ac545dd29
Andre Cruz <andreldcruz@outlook.com>
2022-12-01 23:30:10 -0300

    exercicios utilizando a API Stream

--- java8/src/LambdaApp.java
+++ java8/src/LambdaApp.java
@@ -1,6 +1,6 @@
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 
 import model.Produto;
@@ -8,7 +8,7 @@ import model.Produto;
 public class LambdaApp {
     public static void main(String[] args) throws Exception {
         Scanner arquivo = new Scanner(new File("Produtos.csv"), "UTF-8");
-        List<Produto> produtos = new ArrayList<>();
+        Collection<Produto> produtos = new LinkedHashSet<>();
 
         while (arquivo.hasNextLine()) {
             Scanner itens = new Scanner(arquivo.nextLine());
@@ -22,6 +22,12 @@ public class LambdaApp {
         }
         System.out.println(
                 "--------------------------------------------------------------------------------------------");
+        System.out.println("Todos os produtos");
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
+        produtos.forEach(System.out::println);
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
         System.out.println("Produtos Dell");
         System.out.println(
                 "--------------------------------------------------------------------------------------------");
@@ -33,6 +39,23 @@ public class LambdaApp {
                 "--------------------------------------------------------------------------------------------");
         produtos.stream().filter(p -> p.getQuantidade() > 10)
                 .sorted((p1, p2) -> Integer.compare(p1.getQuantidade(), p2.getQuantidade()))
+                .forEach(System.out::println);
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
+        System.out.println("Valor Total produtos Dell");
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
+        System.out.println("Quantidade: " + produtos.stream().filter(p -> p.getMarca().equalsIgnoreCase("dell"))
+                .mapToInt(Produto::getQuantidade).sum());
+        System.out.println("Valor total: R$ " + produtos.stream().filter(p -> p.getMarca().equalsIgnoreCase("dell"))
+                .mapToDouble(Produto::getValor).sum());
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
+        System.out.println("Notebooks 14 polegadas");
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
+        produtos.stream()
+                .filter(p -> p.getNome().toLowerCase().contains("14"))
                 .forEach(System.out::println);
 
         arquivo.close();
